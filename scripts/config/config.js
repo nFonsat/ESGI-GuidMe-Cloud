76f733741c6761fc105ec885fc55ff1167848c8c
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-11-09 23:05:37 +0100

    add config db

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -3,5 +3,9 @@ module.exports = {
 	port: "8080",
 	api: {
 		base_url: "api/v1"
-	}
+	},
+    database: {
+        uri: 'mongodb://nicolas:2016&EsGi@127.0.0.1:2707/GuidMeESGI',
+        secret: 'hackaton&guidMe?Mobilite:ESGI;'
+    }
 }
