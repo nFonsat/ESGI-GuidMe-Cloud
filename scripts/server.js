ff28c62f7456d45fac8561ddf22e8a7fb282356a
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-11-10 21:28:36 +0100

    implement server script

--- scripts/server.js
+++ scripts/server.js
@@ -1,18 +1,39 @@
 "use strict";
 
-var express = require('express');
-var mongoose = require('mongoose');
-var morgan = require('morgan');
-
 var config = require('./config/config.js');
 
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var jwt         = require('jsonwebtoken');
+
 var app = express();
+
+//Config Mongoose DB
+var appConnection = mongoose.createConnection(config.database.uri);
+appConnection.on('error', console.error.bind(console, 'connection error:'));
+appConnection.once('open', function callback () {
+  console.log("Connection to database etablish");
+});
+
+app.set('superSecret', config.database.secret);
+
+//Config Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Config Morgan
+app.use(morgan('dev'));
+
+//Router
+app.get('/', function(req, res) {
+    res.send('Hello!');
+});
 
 var apiRoutes = express.Router();
 
 app.use(config.api.base_url, apiRoutes);
-
-app.use(morgan('dev'));
 
 app.listen(config.port, function () {
 	console.log("Cloud Guid Me is running on port %s", config.port);
