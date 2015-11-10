a5b4d7b3eddf7d20265bf7c27c7d54ace44e4a5f
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-11-10 21:28:20 +0100

    Change db config

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
 		base_url: "api/v1"
 	},
     database: {
-        uri: 'mongodb://nicolas:2016&EsGi@127.0.0.1:2707/GuidMeESGI',
+        uri: 'mongodb://localhost:27017/GuidMeESGI',
         secret: 'hackaton&guidMe?Mobilite:ESGI;'
     }
 }
