898f04695c5f1fe606bfcca620181bd6d4641995
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-11-11 11:34:59 +0100

    fix bug utils method

--- scripts/utils/utils.js
+++ scripts/utils/utils.js
@@ -1,13 +1,35 @@
-module.exports =  {
-	guuid = function () {
-		function s4() {
-		    return Math.floor((1 + Math.random()) * 0x10000)
-		      .toString(16)
-		      .substring(1);
-			}
-		
-		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-		    s4() + '-' + s4() + s4() + s4();
-		}
-	};
-}
+exports.guuid = function () {
+	function s4() {
+        var date = new Date();
+	    return Math.floor((1 + Math.random()) * 0x10000 * date.getTime())
+	      .toString(4)
+	      .substring(1);
+	}
+	
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+	    s4() + '-' + s4() + s4() + s4();
+};
+
+exports.listRoutes = function () {
+	function space(x) {
+	    var res = '';
+	    while(x--) res += ' ';
+	    return res;
+	}
+
+	for (var i = 0; i < arguments.length;  i++) {
+        if(arguments[i].stack instanceof Array){
+            console.log('');
+            arguments[i].stack.forEach(function(a){
+                var route = a.route;
+                if(route){
+                    route.stack.forEach(function(r){
+                        var method = r.method.toUpperCase();
+                        console.log(method,space(8 - method.length),route.path);
+                    })
+                }
+            });
+            console.log('');
+        }
+    }
+};
