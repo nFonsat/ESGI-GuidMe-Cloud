cea68cd8d0d047b77c398a9481ce42fad75a03a8
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-11-12 20:30:21 +0100

    add oauth2-server module

--- scripts/server.js
+++ scripts/server.js
@@ -7,7 +7,6 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
-var jwt         = require('jsonwebtoken');
 
 var app = express();
 
