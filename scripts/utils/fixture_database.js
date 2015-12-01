685490060ded1f2400c4d3bd94c54c35360a6455
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-01 22:40:31 +0100

    add new fixture

--- scripts/utils/fixture_database.js
+++ scripts/utils/fixture_database.js
@@ -1,22 +1,38 @@
 "use strict";
+var clients                 = require('../config/config.js').clients,
+    OAuthClientOAuthModel   = require('../schemas/oauthClientSchema'),
+    User                    = require('../schemas/userSchema'),
+    mongoose                = require('mongoose');
 
 exports.loadOAuthClientFixture = function () {
-    var clients                 = require('../config/config.js').clients,
-        OAuthClientOAuthModel   = require('../schemas/oauthClientSchema'),
-        mongoose                = require('mongoose');
-
-        clients.forEach(function (client) {
-            var clientDB = new OAuthClientOAuthModel({
-                clientId: client.clientId,
-                clientSecret: client.clientSecret
-            });
-
-            clientDB.save(function (err) {
-                if (err) {
-                    throw err;
-                }
-
-                console.log("Client %s added to database", client.clientName);
-            });
+    clients.forEach(function (client) {
+        var clientDB = new OAuthClientOAuthModel({
+            clientId: client.clientId,
+            clientSecret: client.clientSecret
         });
+
+        clientDB.save(function (err) {
+            if (err) {
+                throw err;
+            }
+
+            console.log("Client %s added to database", client.clientName);
+        });
+    });
+};
+
+exports.loadUserFixture = function() {
+    var testUser = new User({
+        username: 'test',
+        password: 'password',
+        email: 'test@test.com'
+    });
+
+    testUser.save(function (err) {
+        if (err) {
+            throw err;
+        }
+
+        console.log("User test added to database");
+    });
 };
