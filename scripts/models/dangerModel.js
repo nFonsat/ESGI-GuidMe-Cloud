77a4ed112c96bc34b0eb3ce5d3ef3397bc3cefe6
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-01-12 00:19:21 +0100

    restructure

--- scripts/models/dangerModel.js
+++ scripts/models/dangerModel.js
@@ -4,42 +4,16 @@ var DangerModel     = module.exports,
     CoordinateModel = require('../models/coordinateModel'),
     DangerSchema    = require('../schemas/dangerSchema');
 
-DangerModel.saveWithLatLng = function(name, userId, latitude, longitude, callback) {
-    CoordinateModel.saveCoordinate(latitude, longitude, function (err, coordinate) {
-        if (err) {
-            console.log("CoordinateModel Error save : %s", err);
-            callback(err);
-        }
-        else {
-            
-        }
-    })
-};
+DangerModel.saveWithLatLng = function(name, userId, latitude, longitude, callback){};
 
-DangerModel.getAddressFromCoordinate = function(userId, latitude, longitude, callback) {
+DangerModel.getAddressFromCoordinate = function(userId, latitude, longitude, callback){};
 
-};
+DangerModel.getAddressByCoordinateId = function(userId, coordinateId, callback){};
 
-DangerModel.getAddressByCoordinateId = function(userId, coordinateId, callback) {
+DangerModel.getAddressesByUserId = function(userId, callback){};
 
-};
+DangerModel.getAddressesById = function(addressId, callback){};
 
-DangerModel.getAddressesByUserId = function(userId, callback) {
-    DangerSchema.find({user:userId})
-                  .populate('coordinate')
-                  .exec(callback);
-};
+DangerModel.updateAddress = function(addressId, newName, callback){};
 
-DangerModel.getAddressesById = function(addressId, callback) {
-    DangerSchema.findById(addressId)
-                  .populate('coordinate')
-                  .exec(callback);
-};
-
-DangerModel.updateAddress = function(addressId, newName, callback) {
-    
-};
-
-DangerModel.deleteAddress = function(addressId, callback) {
-    DangerSchema.findByIdAndRemove(addressId, callback);
-};
+DangerModel.deleteAddress = function(addressId, callback){};
