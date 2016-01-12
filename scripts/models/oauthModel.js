c7d1e07d4ce013a273a95b0d66f85dcb02ec6193
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-01-12 15:37:13 +0100

    oauthCLient is unique

--- scripts/models/oauthModel.js
+++ scripts/models/oauthModel.js
@@ -9,72 +9,110 @@ var OAuthModel              = module.exports,
 
 
 OAuthModel.getAccessToken = function (bearerToken, callback) {
-  console.log('in getAccessToken (bearerToken: ' + bearerToken + ')');
+    console.log('in getAccessToken (bearerToken: ' + bearerToken + ')');
 
-  OAuthAccessTokenSchema.findOne({ accessToken: bearerToken }, callback);
+    OAuthAccessTokenSchema.findOne({ accessToken: bearerToken }, callback);
 };
 
 OAuthModel.getClient = function (clientId, clientSecret, callback) {
-  console.log('in getClient (clientId: ' + clientId + ', clientSecret: ' + clientSecret + ')');
+    console.log('in getClient (clientId: ' + clientId + ', clientSecret: ' + clientSecret + ')');
 
-  OAuthClientSchema.findOne({ clientId: clientId, clientSecret: clientSecret }, callback);
+    OAuthClientSchema.findOne({ clientId: clientId, clientSecret: clientSecret }, callback);
 };
 
+OAuthModel.saveClient = function(clientId, clientSecret, callback) {
+    var clientDB = new OAuthClientSchema({
+        clientId: clientId,
+        clientSecret: clientSecret
+    });
+
+    OAuthModel.findOneByClientIdOrClientSecret(clientId, clientSecret, 
+        function(err, result){
+            if (err) {
+                callback(err);
+            }
+            else if (result) {
+                var error = "OAuthClient " + result.clientId + " already exists";
+                callback(error);
+            }
+            else {
+                clientDB.save(callback);
+            }
+        }
+    );
+}
+
+OAuthModel.findOneByClientIdOrClientSecret = function(clientId, clientSecret, callback) {
+    OAuthClientSchema.findOne( { $or: [ { clientId: clientId }, { clientSecret: clientSecret } ] }, 
+        function(err, result){
+            if (err){
+                callback(err);
+            }
+            else {
+                callback(null, result);
+            }
+        }
+    );
+}
+
 OAuthModel.grantTypeAllowed = function (clientId, grantType, callback) {
-  console.log('in grantTypeAllowed (clientId: ' + clientId + ', grantType: ' + grantType + ')');
-  callback(false, true);
+    console.log('in grantTypeAllowed (clientId: ' + clientId + ', grantType: ' + grantType + ')');
+    callback(false, true);
 };
 
 OAuthModel.saveAccessToken = function (token, clientId, expires, userId, callback) {
-  userId = (typeof(userId) === "object") ? userId.id : userId;
-  console.log('in saveAccessToken (token: ' + token + ', clientId: ' + clientId + ', userId: ' + userId + ', expires: ' + expires + ')');
+    userId = (typeof(userId) === "object") ? userId.id : userId;
+    console.log('in saveAccessToken (token: ' + token + ', clientId: ' + clientId + ', userId: ' + userId + ', expires: ' + expires + ')');
 
-  var accessToken = new OAuthAccessTokenSchema({
-    accessToken: token,
-    clientId: clientId,
-    userId: userId,
-    expires: expires
-  });
+    var accessToken = new OAuthAccessTokenSchema({
+        accessToken: token,
+        clientId: clientId,
+        userId: userId,
+        expires: expires
+    });
 
-  accessToken.save(callback);
+    accessToken.save(callback);
 };
 
 /*
  * Required to support password grant type
  */
 OAuthModel.getUser = function (username, password, callback) {
-  console.log('in getUser (username: ' + username + ', password: ' + password + ')');
+    console.log('in getUser (username: ' + username + ', password: ' + password + ')');
 
-  var hashPassword = SHA512(password);
-  UserSchema.findOne({ username: username, password: hashPassword.toString() }, function(err, user) {
-    if(err) {
-        console.log("Error getUser : %s", err);
-        return callback(err);
-    } 
-    console.log(user);
-    callback(null, user.id);
-  });
+    var hashPassword = SHA512(password);
+    UserSchema.findOne({ username: username, password: hashPassword.toString() }, 
+        function(err, user) {
+            if(err) {
+                console.log("Error getUser : %s", err);
+                return callback(err);
+            } 
+            
+            console.log(user);
+            callback(null, user.id);
+        }
+    );
 };
 
 /*
  * Required to support refreshToken grant type
  */
 OAuthModel.saveRefreshToken = function (token, clientId, expires, userId, callback) {
-  userId = (typeof(userId) === "object") ? userId.id : userId;
-  console.log('in saveRefreshToken (token: ' + token + ', clientId: ' + clientId +', userId: ' + userId + ', expires: ' + expires + ')');
+    userId = (typeof(userId) === "object") ? userId.id : userId;
+    console.log('in saveRefreshToken (token: ' + token + ', clientId: ' + clientId +', userId: ' + userId + ', expires: ' + expires + ')');
 
-  var refreshToken = new OAuthRefreshTokenSchema({
-    refreshToken: token,
-    clientId: clientId,
-    userId: userId,
-    expires: expires
-  });
+    var refreshToken = new OAuthRefreshTokenSchema({
+        refreshToken: token,
+        clientId: clientId,
+        userId: userId,
+        expires: expires
+    });
 
-  refreshToken.save(callback);
+    refreshToken.save(callback);
 };
 
 OAuthModel.getRefreshToken = function (refreshToken, callback) {
-  console.log('in getRefreshToken (refreshToken: ' + refreshToken + ')');
+    console.log('in getRefreshToken (refreshToken: ' + refreshToken + ')');
 
-  OAuthRefreshTokenSchema.findOne({ refreshToken: refreshToken }, callback);
+    OAuthRefreshTokenSchema.findOne({ refreshToken: refreshToken }, callback);
 };
--- scripts/utils/fixture_database.js
+++ scripts/utils/fixture_database.js
@@ -1,41 +1,40 @@
 "use strict";
 
-var clients                 = require('../config/config.js').clients,
-    dangerTypes             = require('../config/config.js').danger_types,
-    OAuthClientOAuthModel   = require('../schemas/oauthClientSchema'),
-    DangerTypeModel         = require('../models/dangerTypeModel'),
-    UserModel               = require('../models/userModel'),
-    mongoose                = require('mongoose');
+var clients             = require('../config/config.js').clients,
+    dangerTypes         = require('../config/config.js').danger_types,
+    OAuthClientModel    = require('../models/oauthModel'),
+    DangerTypeModel     = require('../models/dangerTypeModel'),
+    UserModel           = require('../models/userModel'),
+    mongoose            = require('mongoose');
 
 exports.loadOAuthClientFixture = function () {
     clients.forEach(function (client) {
-        var clientDB = new OAuthClientOAuthModel({
-            clientId: client.clientId,
-            clientSecret: client.clientSecret
-        });
-
-        clientDB.save(function (err) {
-            if (err) {
-                console.log(err);
+        OAuthClientModel.saveClient(client.clientId, client.clientSecret, 
+            function(err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("Client %s added to database", client.clientName);
+                }
             }
-            else {
-                console.log("Client %s added to database", client.clientName);
-            }
-        });
+        );
     });
 };
 
 exports.loadDangerTypeFixture = function () {
     dangerTypes.forEach(function (type) {
 
-        DangerTypeModel.save(type.name, type.icon, function(err, type) {
-            if (err) {
-                console.log(err);
+        DangerTypeModel.save(type.name, type.icon, 
+            function(err, type) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("Danger Type %s added to database", type.name);
+                }
             }
-            else {
-                console.log("Danger Type %s added to database", type.name);
-            }
-        });
+        );
     });
 };
 
@@ -46,7 +45,7 @@ exports.loadUserFixture = function() {
                 console.log(err);
             }
             else {
-                console.log("User " + result.username + " added to database");
+                console.log("User %s added to database", result.username);
             }
         }
     );
