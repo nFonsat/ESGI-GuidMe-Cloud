125a03cb7ccb91bc65996618dc7b163fcb62ec42
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-01-12 01:11:18 +0100

    implement danger model

--- scripts/models/dangerModel.js
+++ scripts/models/dangerModel.js
@@ -2,18 +2,73 @@
 
 var DangerModel     = module.exports,
     CoordinateModel = require('../models/coordinateModel'),
+    DangerTypeModel = require('../models/dangerTypeModel'),
     DangerSchema    = require('../schemas/dangerSchema');
 
-DangerModel.saveWithLatLng = function(name, userId, latitude, longitude, callback){};
+DangerModel.save = function(name, typeId, userId, latitude, longitude, callback){
+    CoordinateModel.saveCoordinate(latitude, longitude, function (err, coordinate) {
+        if (err) {
+            callback(err);
+        }
+        else {
+            var danger = new DangerSchema({
+                name: name,
+                type: typeId,
+                user: userId,
+                coordinate: coordinate.id
+            });
 
-DangerModel.getAddressFromCoordinate = function(userId, latitude, longitude, callback){};
+            danger.save(callback);
+        }
+    })
+};
 
-DangerModel.getAddressByCoordinateId = function(userId, coordinateId, callback){};
+DangerModel.findById = function(dangerId, callback){
+        DangerSchema.findById(dangerId, callback);
+};
 
-DangerModel.getAddressesByUserId = function(userId, callback){};
+DangerModel.findByDistanceToPoint = function(latitude, longitude, distance, callback){
+    CoordinateModel.findAllCoordinate(latitude, longitude, distance, 100, function(err, coordinates){
+        if (err) {
+            callback(err);
+        }
+        else {
+            callback(null, coordinates);
+        }
+    });
+}
 
-DangerModel.getAddressesById = function(addressId, callback){};
+DangerModel.findByUserId = function(userId, callback){
+    DangerSchema.find({user:userId})
+                .populate('type')
+                .populate('coordinate')
+                .exec(callback);
+};
 
-DangerModel.updateAddress = function(addressId, newName, callback){};
+DangerModel.update = function(dangerId, userId, newName, newType, callback){
+    DangerSchema.findById(addressId, function (err, danger) {
+        if (err){
+            callback(err);
+        }
+        else if (danger.id == userId) {
+            danger.name = newName;
+            danger.type = newType;
+            danger.save(callback);
+        } else {
+            callback("User not owner");
+        }
+    });
+};
 
-DangerModel.deleteAddress = function(addressId, callback){};
+DangerModel.delete = function(dangerId, userId, callback){
+    DangerSchema.findById(addressId, function (err, danger) {
+        if (err){
+            callback(err);
+        }
+        else if (danger.id == userId){
+            danger.remove(callback);
+        } else {
+            callback("User not owner");
+        }
+    });
+};
